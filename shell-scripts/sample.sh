2047b808b7dd13e398c8383429075d319ab0b61f
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-02-18 09:36:14 +0100

    fix sample

--- shell-scripts/sample.sh
+++ shell-scripts/sample.sh
@@ -4,14 +4,14 @@
 find . -name '*.java' -type f -exec wc -l '{}' + | sort -rn | awk '$1>50'
 
 # Variablen
-C = 5
-message = 'Hello'
+C=5
+message='Hello'
 
 echo $message #Zugriff auf eine Variable mit $<var>
 
 # Schleifen
 
-while [$C -lt 10]; do
+while [ $C -lt 10 ]; do
   ((C++))
   echo $C
 
@@ -19,8 +19,7 @@ done
 
 # Abfragen
 
-if [$C -gt 20]; then echo 'C was greater than 20' fi
-if [$C -gt 5]; then 
-   echo 'C was greater than 5' 
+if [ $C -gt 20 ]; then echo 'C was greater than 20'; fi
+if [ $C -gt 5 ]; then 
+   echo 'C was greater than 5';
 fi
-
